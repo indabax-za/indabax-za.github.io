b24ba1af49cb3d739809602ecdacc9d1b999f873
sicelukwanda <sicelukwanda.zwane@gmail.com>
2019-04-12 16:19:27 +0200

    Disabled single tab view in 2018-schedule

--- assets/js/main.js
+++ assets/js/main.js
@@ -872,24 +872,26 @@ function populateOrganizers(organizers) {
 }
 
 // javascript for accordion buttons
-//var acc = document.getElementsByClassName("accordion_button");
-//var i;
+var acc = document.getElementsByClassName("accordion_button");
+var i;
 
-/*for (i = 0; i < acc.length; i++) {
+for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
-   // this.classList.toggle("ac_active");
+    this.classList.toggle("ac_active");
 
     /* Toggle between hiding and showing the active panel */
-   /* var panel = this.nextElementSibling;
+    var panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
     } else {
       panel.style.display = "block";
     }
   });
-}*/
+}
+
+/*
 var acc = document.getElementsByClassName("accordion_button");
 var panel = document.getElementsByClassName('accordion_panel');
 
@@ -912,7 +914,7 @@ function setClass(els, className, fnName) {
     }
 }
 
-
+*/
 function populatePanel(panellists) {
     var $row_div = $("<div />").addClass("row");
 
